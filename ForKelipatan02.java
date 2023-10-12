f1902c30c54c5301e99dfd1b692150b881a4000d
Yuanzth <adityayuhanda3001@gmail.com>
2023-10-12 11:53:06 +0700

    Percobaan 1 Jawaban nomor 3

--- ForKelipatan02.java
+++ ForKelipatan02.java
@@ -8,16 +8,15 @@ public class ForKelipatan02 {
         counter = 0;
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
         kelipatan = scan.nextInt();
-        for (int i = 1 ; i <= 50 ; i++)
-        {
-            if (i % kelipatan == 0)
-            {
-                jumlah += 1;
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
                 counter++;
             }
         }
-        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+            double rataRata = (double) jumlah / counter;
+            System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+            System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+            System.out.printf("Rata-rata bilangan kelipatan %d dari 1 sampai 50 adalah %.2f\n", kelipatan, rataRata);        
     }
-    
 }
