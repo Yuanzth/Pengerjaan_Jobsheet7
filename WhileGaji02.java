3fe03330841576b950cc9fa15a8026e8fb3c095f
Yuanzth <adityayuhanda3001@gmail.com>
2023-10-12 13:04:53 +0700

    Percobaan 2

---
+++ WhileGaji02.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class WhileGaji02 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur, totalGajiLembur;
+        String jabatan;
+        gajiLembur = 0;
+        totalGajiLembur = 0;
+
+        System.out.print("Masukkan Jumlah Karyawan : ");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+        while (i < jumlahKaryawan) 
+        {
+            System.out.println("Pilihan Jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan ke-" + (i+1) + ": ");
+            jabatan = scan.next();
+            System.out.print("Masukkan Jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+        
+            if (jabatan.equalsIgnoreCase("direktur"))
+            {
+                continue;
+            }
+            else if (jabatan.equalsIgnoreCase("manajer"))
+            {
+                gajiLembur = jumlahJamLembur * 100000;
+            }
+            else if (jabatan.equalsIgnoreCase("karyawan"))
+            {
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+            totalGajiLembur += gajiLembur;
+        }
+        System.out.println("Total gaji lembur : " + totalGajiLembur);
+    }
+
+}
